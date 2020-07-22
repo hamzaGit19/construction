57ed6502171445bee45dfaad90183cc78c200cf2
Shahwar Khalid <itisshahwar@gmail.com>
2020-07-22 22:26:23 +0500

    removed navbar dependency

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ require("@rails/activestorage").start()
 require("channels")
 require('jquery')
 require("src/cocoon")
-require("src/navbar")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
