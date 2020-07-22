c66b457cc6ab91f43afaa45784e1eafd5747ad08
Hamza Awais <hamza.awais@square63.org>
2020-07-22 21:44:40 +0500

    Navbar update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def is_active(action, controller)
+    if params[:action] == action && params[:controller] == controller
+      'selected'
+    else
+      nil
+    end
+  end
 end
